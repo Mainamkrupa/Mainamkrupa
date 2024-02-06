da7c2aab815f73951b3c1b825779a094a77258af
krupamainam <mainamkrupa145@gmail.com>
2024-02-06 23:46:17 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,24 @@
+function addTask() {
+    var taskInput = document.getElementById("taskInput");
+    var taskList = document.getElementById("taskList");
+
+    if (taskInput.value.trim() === "") {
+        alert("Please enter a task!");
+        return;
+    }
+
+    var li = document.createElement("li");
+    li.innerHTML = `
+        <span>${taskInput.value}</span>
+        <button onclick="removeTask(this)">Remove</button>
+    `;
+
+    taskList.appendChild(li);
+    taskInput.value = "";
+}
+
+function removeTask(button) {
+    var taskList = document.getElementById("taskList");
+    var li = button.parentNode;
+    taskList.removeChild(li);
+}
